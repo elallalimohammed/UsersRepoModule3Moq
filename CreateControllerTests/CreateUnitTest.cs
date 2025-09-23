2e6918131a582601f228b8cdb6b94421e8857d18
M-Elallali <50343573+elallalimohammed@users.noreply.github.com>
2025-09-23 20:44:25 +0200

    Update CreateUnitTest.cs

--- CreateControllerTests/CreateUnitTest.cs
+++ CreateControllerTests/CreateUnitTest.cs
@@ -10,7 +10,7 @@ namespace CreateControllerTests;
 public class CreateUnitTest
 {
   
-   /// <returns></returns>
+   
     [TestMethod]
     public async Task Create_ReturnsCreatedAtAction_WhenModelIsValid()
     {
